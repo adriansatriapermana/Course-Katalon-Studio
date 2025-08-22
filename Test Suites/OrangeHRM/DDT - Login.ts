1043fd0b889e9f26539d9c49f4fba4f7c58af6c4
Adrian <adrian.satria.permana@gmail.com>
2025-08-22 17:39:26 +0700

    push login ddt

---
+++ Test Suites/OrangeHRM/DDT - Login.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DDT - Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2789dc91-2621-40cc-82ed-fdfa1fbd78b0</testSuiteGuid>
+   <testCaseLink>
+      <guid>7e396076-df1c-44c8-8089-1e246f9cd1ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeHRM/Data Drive Test - DDT Login OrangeHRM</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>61ea853b-02e0-4e6f-a2ac-4fb64b148c8b</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/OrangeHRM/DDT - Login (1)</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>61ea853b-02e0-4e6f-a2ac-4fb64b148c8b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>b5811722-532d-4f7d-afde-29197cf73a37</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>61ea853b-02e0-4e6f-a2ac-4fb64b148c8b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>c2b985a3-bfe8-477e-b15a-a942ade39102</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>61ea853b-02e0-4e6f-a2ac-4fb64b148c8b</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>expected</value>
+         <variableId>4a37971d-be51-4830-9587-dad2f39051ca</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
